b648a4e98dab00090a3d52234e9c1c19dfe08c0c
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-02-28 11:22:09 +0400

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second) + multiply(first, second);
+    }
+
+    public static double subtractDivide(double first, double second) {
+        return subtract(first, second) - divide(first, second);
+    }
+
+    public static double crazyCalc(double first, double second) {
+        return sum(first, second) + multiply(first, second) - subtract(first, second) - divide(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(20, 10));
+        System.out.println("Результат расчета равен: " + subtractDivide(20, 10));
+        System.out.println("Результат расчета равен: " + crazyCalc(20, 10));
+    }
+}
