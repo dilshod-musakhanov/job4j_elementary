3bc2cd596cc8ce70571fd7aed52d83ccfbe45710
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-03-04 09:29:02 +0400

    4.5. Отладка программы в IDEA [#159464 #271297]

--- src/test/java/ru/job4j/condition/ThreeMaxTest.java
+++ src/test/java/ru/job4j/condition/ThreeMaxTest.java
@@ -45,4 +45,33 @@ public class ThreeMaxTest {
         Assert.assertEquals(expected, result);
     }
 
+    @Test
+    public void firstEqSecond() {
+        int first = 10;
+        int second = 10;
+        int third = 1;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 10;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void firstEqThird() {
+        int first = 100;
+        int second = 1;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void secondEqThird() {
+        int first = 1;
+        int second = 100;
+        int third = 100;
+        int result = ThreeMax.max(first, second, third);
+        int expected = 100;
+        Assert.assertEquals(expected, result);
+    }
 }
